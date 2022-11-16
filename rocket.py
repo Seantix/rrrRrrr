ad13de284b4c435f8ca3a15b7675f76167dc298c
Seantix <113047459+Seantix@users.noreply.github.com>
2022-11-16 11:40:55 +0300

    Update rocket.py

--- rocket.py
+++ rocket.py
@@ -3,15 +3,38 @@ import numpy as np
 
 MODEL_G = 9.8
 MODEL_DT = 0.1
-class Rocket:
+class Body:
     def __init__(self, x, y, vx, vy):
         """
-        Создаем класс Ракеты:
+        Создаем класс Тела:
 
         x - x-координата
         y - y-координата
         vx - проекция скорости на Ox
         vy - проекция скорости на Oy
+        """
+        
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.traj_x = []
+        self.traj_y = []
+    
+    def advance(self):
+        self.vy -= MODEL_G * MODEL_DT
+        self.y += self.vy * MODEL_DT
+        self.x += self.vx * MODEL_DT
+        self.traj_x.append(self.x)
+        self.traj_y.append(self.y)
+
+
+class Rocket(Body):
+
+    def __init__(self, x, y, vx, vy):
+        """
+        Создаем класс Ракеты:
+
         angle - наклон ракеты относительно Ох
         mass - масса
         fuel_cons - расход топлива
@@ -19,48 +42,29 @@ class Rocket:
 
         Данные для последних трех свойств взяты с ракеты Фау-2
         """
-        self.x = x
+        super().__init__(x, y, vx, vy)
         self.angle = np.pi / 4
-        self.y = y
-        self.vx = vx
-        self.vy = vy
         self.mass = 12500
         self.fuel_cons = 127
-        self.fuel_v = 2050
-        self.traj_x = []
-        self.traj_y = []
+        self.fuel_v = 2050       
 
     def advance(self):
-        """
-        Метод для "ракетного полета". Проекции скоростей меняются согласно уравнению Мещерского, масса уменьшается очевидным образом.
-        """
-        self.vy += MODEL_DT * (self.mass * MODEL_G + self.fuel_cons * self.fuel_v * np.sin(self.angle)) / self.mass
+        super().advance()
+        if self.mass < 4000:
+            return
+        self.vy += MODEL_DT * (self.fuel_cons * self.fuel_v * np.sin(self.angle)) / self.mass
         self.vx += MODEL_DT * (self.fuel_cons * self.fuel_v * np.cos(self.angle)) / self.mass
-        self.x += self.vx * MODEL_DT
-        self.y += self.vy * MODEL_DT
         self.mass -= self.fuel_cons * MODEL_DT
-        self.traj_x.append(self.x)
-        self.traj_y.append(self.y)
-    
-    def free(self):
-        """
-        Метод для "свободного" полета и падения, т.е. когда топливо кончилось.
-        """
-        self.vy -= MODEL_G * MODEL_DT
-        self.y += self.vy * MODEL_DT
-        self.x += self.vx * MODEL_DT
-        self.traj_x.append(self.x)
-        self.traj_y.append(self.y)
+
+
 
 r = Rocket(0, 0, 0, 0)
+b = Body(0, 0, 10, 10)
 
-for i in np.arange (0, 200, MODEL_DT):
-    while r.y >= 0:
-        if r.mass > 4000:
-            r.advance()
-        else:
-            r.free()
+for bb in [r, b]:
+    for i in np.arange (0, 2, MODEL_DT):
+        bb.advance()
+    pp.plot(bb.traj_x, bb.traj_y)
 
 
-pp.plot(r.traj_x, r.traj_y)
 pp.show()
